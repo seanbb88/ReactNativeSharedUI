e9cb7084f84555e86888c08c929e8ffa1792091f
Sean Brown <sbbrown35@gmail.com>
2023-12-27 13:15:12 -0500

    try

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+
       <AppButton onPress={() => { }} text="Button" buttonStyle={{ backGroundColor: "#E0E0E0", borderColor: "#255433", textColor: '#255433' }} />
     </View>
   );
